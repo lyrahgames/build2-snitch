8960686bfe0bab0ee32420569a39b23db0619d29
lyrahgames <lyrahgames@mailbox.org>
2024-02-23 21:46:45 +0100

    Fix multiple definition of `main`

--- libsnitch/src/snitch_main.cpp
+++ libsnitch/src/snitch_main.cpp
@@ -1,6 +1,7 @@
 #include "snitch/snitch_cli.hpp"
 #include "snitch/snitch_registry.hpp"
 
+#if SNITCH_DEFINE_MAIN
 int main(int argc, char* argv[]) {
     std::optional<snitch::cli::input> args = snitch::cli::parse_arguments(argc, argv);
     if (!args) {
@@ -11,3 +12,4 @@ int main(int argc, char* argv[]) {
 
     return snitch::tests.run_tests(*args) ? 0 : 1;
 }
+#endif
