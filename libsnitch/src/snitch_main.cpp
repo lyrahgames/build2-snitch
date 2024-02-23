dca4f5851768734b7247df67a874f42a9ab24bdb
lyrahgames <lyrahgames@mailbox.org>
2024-02-23 17:54:23 +0100

    Fix symbol exportation for `main` function

--- libsnitch/src/snitch_main.cpp
+++ libsnitch/src/snitch_main.cpp
@@ -1,1 +1,13 @@
-../../upstream/src/snitch_main.cpp
+#include "snitch/snitch_cli.hpp"
+#include "snitch/snitch_registry.hpp"
+
+int main(int argc, char* argv[]) {
+    std::optional<snitch::cli::input> args = snitch::cli::parse_arguments(argc, argv);
+    if (!args) {
+        return 1;
+    }
+
+    snitch::tests.configure(*args);
+
+    return snitch::tests.run_tests(*args) ? 0 : 1;
+}
